5fac31121441b65a145f91921f6d341d79076cf0
devstorm91-cell <devstorm91@gmail.com>
2026-07-20 19:47:16 +0530

    Time: 33 ms (69.45%), Space: 73.2 MB (22.45%) - LeetHub

---
+++ 0895-maximum-frequency-stack/0895-maximum-frequency-stack.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class FreqStack {
+
+    private HashMap<Integer, Integer> freq;
+    private HashMap<Integer, Stack<Integer>> group;
+    private int maxFreq;
+
+    public FreqStack() {
+        freq = new HashMap<>();
+        group = new HashMap<>();
+        maxFreq = 0;
+    }
+
+    public void push(int val) {
+
+        int f = freq.getOrDefault(val, 0) + 1;
+        freq.put(val, f);
+
+        if (f > maxFreq)
+            maxFreq = f;
+
+        group.putIfAbsent(f, new Stack<>());
+        group.get(f).push(val);
+    }
+
+    public int pop() {
+
+        int val = group.get(maxFreq).pop();
+
+        freq.put(val, freq.get(val) - 1);
+
+        if (group.get(maxFreq).isEmpty())
+            maxFreq--;
+
+        return val;
+    }
+}
