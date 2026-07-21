3359d4d329ba29f403733843c81cd78f294d3941
devstorm91-cell <devstorm91@gmail.com>
2026-07-21 06:46:13 +0530

    Time: 0 ms (100%), Space: 43 MB (86.37%) - LeetHub

---
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+        int[] counts=new int[2];
+        for(int student: students){
+            counts[student]++;
+        }
+        for(int sandwich: sandwiches){
+            if(counts[sandwich]==0){
+                break;
+            }
+            counts[sandwich]--;
+        }
+        return counts[0]+counts[1];
+    }
+}
